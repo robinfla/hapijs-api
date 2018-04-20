3db7ea5c9693074fad7a65b8103cfa2cd45ca2f7
Alexis Ohayon <alexis@thefamily.co>
2018-04-20 12:04:42 +0200

    Added dependencies

--- src/api.js
+++ src/api.js
@@ -1,12 +1,28 @@
+'user strict'
+
+const Joi = require('joi')
+const Boom = require('boom')
+
 exports.plugin = {
   name: 'lambda',
   version: '1.0.0',
   register: function (server, options) {
     server.route({
-      method: 'GET',
-      path: '/test',
-      handler: function (request, h) {
-        return 'hello, world'
+      method: 'POST',
+      path: '/send',
+      config: {
+        handler: async function (req, h) {
+          try {
+          } catch (err) {
+            return Boom.badImplementation(err)
+          }
+        },
+        validate: {
+          payload: {
+            email: Joi.string().email().required()
+          },
+          failAction: (req, h, err) => err
+        }
       }
     })
   }
